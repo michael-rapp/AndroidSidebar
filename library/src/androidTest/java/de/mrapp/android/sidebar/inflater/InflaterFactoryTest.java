7f25bf6a53e4a7351344b872cbd1501c7dc27fd2
Michael Rapp <michael.rapp90@googlemail.com>
2016-02-24 03:03:53 +0100

    Changed version to Apache License version 2.0.

--- library/src/androidTest/java/de/mrapp/android/sidebar/inflater/InflaterFactoryTest.java
+++ library/src/androidTest/java/de/mrapp/android/sidebar/inflater/InflaterFactoryTest.java
@@ -1,19 +1,15 @@
 /*
- * AndroidSidebar Copyright 2014 Michael Rapp
+ * Copyright 2014 - 2016 Michael Rapp
  *
- * This program is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
- * General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/>. 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package de.mrapp.android.sidebar.inflater;
 
@@ -22,42 +18,39 @@ import android.test.InstrumentationTestCase;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
 import de.mrapp.android.sidebar.R;
 
 /**
  * Tests the functionality of the class {@link InflaterFactory}.
- * 
+ *
  * @author Michael Rapp
  */
 public class InflaterFactoryTest extends InstrumentationTestCase {
 
-	/**
-	 * Tests the functionality of the createInflater-method, if it is used to
-	 * create an inflater, which allows to inflate view, which are present as an
-	 * instance of the class {@link View}.
-	 */
-	public final void testCreateViewInflaterByViewInstance() {
-		Context context = getInstrumentation().getContext();
-		View view = new View(context);
-		ViewGroup viewGroup = new LinearLayout(context);
-		Inflater inflater = InflaterFactory.createInflater(view);
-		View inflatedView = inflater.inflate(context, viewGroup, false);
-		assertEquals(view, inflatedView);
-	}
+    /**
+     * Tests the functionality of the createInflater-method, if it is used to create an inflater,
+     * which allows to inflate view, which are present as an instance of the class {@link View}.
+     */
+    public final void testCreateViewInflaterByViewInstance() {
+        Context context = getInstrumentation().getContext();
+        View view = new View(context);
+        ViewGroup viewGroup = new LinearLayout(context);
+        Inflater inflater = InflaterFactory.createInflater(view);
+        View inflatedView = inflater.inflate(context, viewGroup, false);
+        assertEquals(view, inflatedView);
+    }
 
-	/**
-	 * Tests the functionality of the createInflater-method, if it is used to
-	 * create an inflater, which allows to inflate views, which may be
-	 * referenced by a resource id.
-	 */
-	public final void testCreateViewInflaterByViewId() {
-		int viewId = R.layout.view;
-		ViewGroup viewGroup = new LinearLayout(getInstrumentation()
-				.getContext());
-		Inflater inflater = InflaterFactory.createInflater(viewId);
-		View inflatedView = inflater.inflate(getInstrumentation().getContext(),
-				viewGroup, false);
-		assertNotNull(inflatedView);
-	}
+    /**
+     * Tests the functionality of the createInflater-method, if it is used to create an inflater,
+     * which allows to inflate views, which may be referenced by a resource id.
+     */
+    public final void testCreateViewInflaterByViewId() {
+        int viewId = R.layout.view;
+        ViewGroup viewGroup = new LinearLayout(getInstrumentation().getContext());
+        Inflater inflater = InflaterFactory.createInflater(viewId);
+        View inflatedView = inflater.inflate(getInstrumentation().getContext(), viewGroup, false);
+        assertNotNull(inflatedView);
+    }
 
 }
